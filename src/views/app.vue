7585053264762c921f2aab43d91e71b619b222dd
tonyyls <yulsh@bingosoft.net>
2017-06-15 14:38:21 +0800

    整理工程入口页面

--- src/views/app.vue
+++ src/views/app.vue
@@ -1,14 +1,24 @@
 <template>
-    <div class="flex-column center">
-        <bui-image src="/image/applogo.png" style="width: 200px;height: 200px"></bui-image>
-        <text>BUI-Weex</text>
+    <div class="center">
+        <bui-image src="/image/applogo.png" class="logo"></bui-image>
+        <text class="h1">BUI-Weex</text>
     </div>
 </template>
 
+<!--引入bui-weex样式文件-->
 <style lang="sass" src="bui-weex/src/css/buiweex.scss"></style>
-<style lang="sass" src="bui-weex/src/css/example.scss"></style>
+
+<style>
+    .logo{
+        width: 200px;
+        height: 200px;
+    }
+</style>
 
 <script>
+    //引入bui-weex模块
+    //import buiweex from 'bui-weex'
+
     var buiweex = require("bui-weex");
     module.exports = {
         data: function () {
