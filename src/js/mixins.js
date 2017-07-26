31b3723d4da498e0e985c314f4e04375e24c1073
tonyyls <yulsh@bingosoft.net>
2017-07-26 17:22:24 +0800

    整理模版,对bui-weex引用调整成0.1.8

--- src/js/mixins.js
+++ src/js/mixins.js
@@ -7,7 +7,9 @@ var mixins = {
         'bui-header':buiweex.buiHeader,
         'bui-icon': buiweex.buiIcon,
         'bui-button': buiweex.buiButton,
-        'bui-image':buiweex.buiImage
+        'bui-image':buiweex.buiImage,
+        'bui-content':buiweex.buiContent,
+        'bui-content-scroll':buiweex.buiContentScroll
     },
     methods: {
 
